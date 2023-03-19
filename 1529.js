1e01d5d689590e1bdfed4cdf13aa8fd22280e676
impression-media-backup1 <115135411+impression-media-backup1@users.noreply.github.com>
2023-03-19 20:53:47 +0100

    Automated update

--- 1529.js
+++ 1529.js
@@ -1,4 +1,4 @@
-/* Version: V_06_c - 19.03. 2023 - 20:53:31 */
+/* Version: V_06_c - 19.03. 2023 - 20:53:40 */
 /* installedModules: adform,appnexus,criteo,pubmatic,rubicon,stroeerCore,teads,ix,consentManagementGDPR,pubCommonIdSystem,criteoIdSystem,schain,sharedIdSystem*/
 /* prebid.js v6.5.0
 Updated : 2022-02-01*/
