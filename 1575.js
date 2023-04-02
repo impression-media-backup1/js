c798174197bac14c022d06b0786e26a2d690504c
impression-media-backup1 <115135411+impression-media-backup1@users.noreply.github.com>
2023-04-02 23:51:27 +0200

    Automated update

--- 1575.js
+++ 1575.js
@@ -1,4 +1,4 @@
-/* Version: V_06_d - 02.04. 2023 - 23:51:08 */
+/* Version: V_06_d - 02.04. 2023 - 23:51:20 */
 /* installedModules: adform,appnexus,criteo,pubmatic,rubicon,teads,consentManagementGDPR,pubCommonIdSystem,criteoIdSystem,schain,sharedIdSystem*/
 var selection = document.body.getAttribute('data-hbmasterscript') !== null;if(selection){console.log(' 0 Start:: R2B2 PrebidAdapter refresh');if(typeof AdTrack.PrebidAdapter.refresh === 'function'){AdTrack.PrebidAdapter.refresh(true, 0);console.log(' 0 End:: R2B2 PrebidAdapter refresh');}}else{console.log('start r2b2ScriptCyklus cyklus: 0');var imHbNewR2b2script = document.createElement('script');imHbNewR2b2script.type = 'text/javascript';imHbNewR2b2script.id = 'imHbR2b2script'; imHbNewR2b2script.src = '//delivery.r2b2.cz/hb/im/hybrid.cz';document.getElementsByTagName('head')[0].appendChild(imHbNewR2b2script);console.log('imHbR2b2script appended');}
 /* prebid.js v6.5.0
