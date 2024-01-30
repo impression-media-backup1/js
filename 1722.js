f11cdfef1eafe2dcf31370251908d95b35e7e6ff
impression-media-backup1 <115135411+impression-media-backup1@users.noreply.github.com>
2024-01-30 12:05:12 +0100

    Automated update

--- 1722.js
+++ 1722.js
@@ -1,4 +1,4 @@
-/* Version: V_06_d - 24.12. 2023 - 20:36:16 */ 
+/* Version: V_06_e - 30.01. 2024 - 12:05:04 */ 
         var checkImGlobalScript = document.querySelector('#imGlobalScript') !== null;
         if (checkImGlobalScript) {
             //OK
@@ -7,7 +7,7 @@
             imGlobalScript.type = 'text/javascript';
             imGlobalScript.id = 'imGlobalScript';
             var imHbRandomParam = Math.floor(Math.random() * 10000) + 1;
-            imGlobalScript.src = 'https://cdn.jsdelivr.net/gh/impression-media/js/imPrebidGlobalVariables.min.js?imHbRandomParam='+imHbRandomParam;
+            imGlobalScript.src = 'https://hb.impressionmedia.cz/tmp/imPrebidGlobalVariables2.js?imHbRandomParam='+imHbRandomParam;
             document.getElementsByTagName('head')[0].appendChild(imGlobalScript);
         } 
             var imIpCloudflare = ''; 
@@ -125,7 +125,7 @@ function sasRequest(aliasDivID, s0, s1, s3, s4){
     
     console.log('sasRequest:'+url);
     //1. OK
-    // https://a.iprima.cz/iprima/bserverj/ball/
+    // https://a.iprima.cz/iprima/lserver/bserverj/ball/
     // site=IMM_Femina/
     // section=web_desktop/
     // area=leaderboard-1/
@@ -150,44 +150,19 @@ function sasRequest(aliasDivID, s0, s1, s3, s4){
     //sasXhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     
 
-    if(sasAsync){
-        sasXhr.onreadystatechange = function(){
-          // In local files, status is 0 upon success in Mozilla Firefox
-          if(sasXhr.readyState === XMLHttpRequest.DONE) {
-            var status = sasXhr.status;
-            if (status === 0 || (status >= 200 && status < 400)) {
-              // The request has been completed successfully
-              console.log('async sasXhr.responseText^');          
-              console.log(sasXhr.responseText);
-              //if empty-ads... 
-    //          var sasDoubleQuoteString = JSON.parse(sasXhr.responseText);
-    //          console.log('sasDoubleQuoteString: '+sasDoubleQuoteString);
-    //          var sasString = sasDoubleQuoteString.toString();
-    //          console.log('sasString: '+sasString);
-    //          sasUnitCustomIDsAds[aliasDivID] = sasString.replaceAll('\"','"');
-              sasUnitCustomIDsAds[aliasDivID] = sasXhr.responseText;
-              console.log(sasUnitCustomIDsAds);
-            } else {
-                console.log('error in sasXhr');
-              // Oh no! There has been an error with the request!
-            }
-          }
-        };
-        sasXhr.send();
+
+    sasXhr.send();
+    console.log('no async sasXhr.responseText^');          
+    console.log(sasXhr.responseText);
+    console.log('aliasDivID: '+aliasDivID);
+    if(aliasDivID == 'returnAd'){
+        console.log(sasUnitCustomIDsAds);
+        return sasXhr.responseText;
     }else{
-        sasXhr.send();
-        console.log('no async sasXhr.responseText^');          
-        console.log(sasXhr.responseText);
-        console.log('aliasDivID: '+aliasDivID);
-        if(aliasDivID == 'returnAd'){
-            console.log(sasUnitCustomIDsAds);
-            return sasXhr.responseText;
-        }else{
-            sasUnitCustomIDsAds[aliasDivID] = sasXhr.responseText;
-            console.log(sasUnitCustomIDsAds)
-        }
-        
+        sasUnitCustomIDsAds[aliasDivID] = sasXhr.responseText;
+        console.log(sasUnitCustomIDsAds)
     }
+        
 
 
 //    sasXhr.onload = function () {
@@ -221,7 +196,7 @@ var imHbWonBids = imHbWonBids || [];
 var imHbAsocPlacementMidEnable = imHbAsocPlacementMidEnable || [];
 var imHbPlacementEnable = imHbPlacementEnable || [];
 
-            function imGetCookie(name) {var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)'); return v ? v[2] : null;}; function imSetCookie(name,value,days){var d=new Date;d.setTime(d.getTime()+24*60*60*1000*days);document.cookie=name+'='+value+';domain=.;path=/;expires='+d.toGMTString();};function imTestingParam(t){var n=null,e=[];return location.search.substr(1).split('&').forEach(function(o){(e=o.split('='))[0]===t&&(n=decodeURIComponent(e[1]))}),n};var imtesting = imTestingParam('imtesting');if(imtesting=='start'){imSetCookie('imtesting','dev',1);}if(imtesting=='stop'){imSetCookie('imtesting', '', -1);}function imRemoveTestInfo(){document.getElementById('imtestingInfo').remove();}function imHbUploadConfig(){if(imGetCookie('imtesting')){document.getElementById('imtestingInfo')&&document.getElementById('imtestingInfo').remove();var infoImTesting=document.createElement('div');infoImTesting.id='imtestingInfo',infoImTesting.innerHTML='<a href="https://hb.impressionmedia.cz/administrace/pages/weby.php?openId=0" style="text-derocation:none; color:white;">TESTING MODE</a><span onclick="imRemoveTestInfo();" style="cursor: pointer;border: 1px solid white;border-radius: 20px 2px 2px;padding: 4px 6px 1px 10px;position: absolute; bottom: 2px;right: 2px;">X</span>',infoImTesting.style.cssText="font-size:12px;line-height:18px;z-index:999999;position:fixed;bottom:3px;right:3px;margin-top:-30px;padding:65px 15px 50px 45px;background:#0000003d;color:white;border-radius:3px;border-top-left-radius:200px;border:1px solid white;box-shadow:1px 1px 1px black;",document.body.appendChild(infoImTesting);;var imConfigId = '0'; var imConfigName = 'HBsetup_'}else{var imConfigId = '1722'; var imConfigName = 'HBsetup_ireferatycz_referaty_cz___SAS_testing___2x_bidder_clon_1721__od_24_12_2_23__2023_12_24_2036'};var imHbScript = document.createElement('script');imHbScript.type = 'text/javascript';imHbScript.id = 'imHbConfig';var imHbRandomParam = Math.floor(Math.random() * 10000) + 1;imHbScript.src = 'https://cdn.jsdelivr.net/gh/impression-media-backup1/js/tmp/js/'+imConfigId+'/'+imConfigName+'.min.js?imHbRandomParam='+imHbRandomParam;document.getElementsByTagName('head')[0].appendChild(imHbScript);}
+            function imGetCookie(name) {var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)'); return v ? v[2] : null;}; function imSetCookie(name,value,days){var d=new Date;d.setTime(d.getTime()+24*60*60*1000*days);document.cookie=name+'='+value+';domain=.;path=/;expires='+d.toGMTString();};function imTestingParam(t){var n=null,e=[];return location.search.substr(1).split('&').forEach(function(o){(e=o.split('='))[0]===t&&(n=decodeURIComponent(e[1]))}),n};var imtesting = imTestingParam('imtesting');if(imtesting=='start'){imSetCookie('imtesting','dev',1);}if(imtesting=='stop'){imSetCookie('imtesting', '', -1);}function imRemoveTestInfo(){document.getElementById('imtestingInfo').remove();}function imHbUploadConfig(){if(imGetCookie('imtesting')){document.getElementById('imtestingInfo')&&document.getElementById('imtestingInfo').remove();var infoImTesting=document.createElement('div');infoImTesting.id='imtestingInfo',infoImTesting.innerHTML='<a href="https://hb.impressionmedia.cz/administrace/pages/weby.php?openId=0" style="text-derocation:none; color:white;">TESTING MODE</a><span onclick="imRemoveTestInfo();" style="cursor: pointer;border: 1px solid white;border-radius: 20px 2px 2px;padding: 4px 6px 1px 10px;position: absolute; bottom: 2px;right: 2px;">X</span>',infoImTesting.style.cssText="font-size:12px;line-height:18px;z-index:999999;position:fixed;bottom:3px;right:3px;margin-top:-30px;padding:65px 15px 50px 45px;background:#0000003d;color:white;border-radius:3px;border-top-left-radius:200px;border:1px solid white;box-shadow:1px 1px 1px black;",document.body.appendChild(infoImTesting);;var imConfigId = '0'; var imConfigName = 'HBsetup_'}else{var imConfigId = '1722'; var imConfigName = 'HBsetup_ireferatycz_referaty_cz___SAS_testing___2x_bidder_clon_1721__od_24_12_2_23__2024_01_30_1205'};var imHbScript = document.createElement('script');imHbScript.type = 'text/javascript';imHbScript.id = 'imHbConfig';var imHbRandomParam = Math.floor(Math.random() * 10000) + 1;imHbScript.src = 'https://cdn.jsdelivr.net/gh/impression-media-backup1/js/tmp/js/'+imConfigId+'/'+imConfigName+'.min.js?imHbRandomParam='+imHbRandomParam;document.getElementsByTagName('head')[0].appendChild(imHbScript);}
     //posledni cyklus nepromazavat    
     var selection = document.body.getAttribute('data-hbmasterscript') !== null;
     if (selection) {       
